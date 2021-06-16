122c2f537d65ab5927fb02a4d1204aceeb0988fe
pthapa <thapa.parmeshwor@gmail.com>
2021-06-15 21:52:21 -0400

    Getting Started App Part 4

--- getting-started-app/mysite/polls/models.py
+++ getting-started-app/mysite/polls/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -11,6 +12,7 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- getting-started-app/mysite/polls/urls.py
+++ getting-started-app/mysite/polls/urls.py
@@ -3,8 +3,8 @@ from .import views
 
 app_name = 'polls'
 urlpatterns = [
-    path(route='', view=views.index, name='index'),
-    path(route='<int:question_id>/', view=views.detail, name='detail'),
-    path(route='<int:question_id>/results/', view=views.results, name='results'),
+    path(route='', view=views.IndexView.as_view(), name='index'),
+    path(route='<int:pk>/',view=views.DetailView.as_view(),name='detail'),
+    path(route='<int:pk>/results/',view=views.ResultsView.as_view(),name='results'),
     path(route='<int:question_id>/vote/', view=views.vote, name='vote'),
 ]
